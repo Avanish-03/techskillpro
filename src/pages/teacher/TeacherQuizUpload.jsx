d0a128b5035304d2ec8eefb944e7b7f814bef74a
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2025-06-18 11:04:23 +0530

    ok

--- src/pages/teacher/TeacherQuizUpload.jsx
+++ src/pages/teacher/TeacherQuizUpload.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { toast } from 'react-toastify';
+
 
 const TeacherQuizUpload = () => {
   const [quizData, setQuizData] = useState({
